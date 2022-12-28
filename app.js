74f6c6544bacfa09719531bdf06466036c2fa49c
Omar Franco Garcia <ofranco1205@gmail.com>
2022-12-28 16:29:50 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require ('body-parser')
+const cors =require("cors")
+const { API_VERSION } = require('./constants')
+
+const app= express()
+
+// Import routings
+//..
+
+// Configure Body Parser
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+//Configure static folder
+app.use(express.static("uploads"))
+
+// Configure Header HTTP --CORS
+app.use(cors())
+
+// Configure routings
+//..
+
+module.exports = app
---
+++ constants.js
@@ -0,0 +1,14 @@
+const DB_USER = "encuesta-digital"
+const DB_PASSWORD = "V3HvDu8NZjJLUEAN"
+const DB_HOST = "cluster0.qqjmz.gcp.mongodb.net"
+
+const API_VERSION = "v1"
+const IP_SERVER= "localhost"
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+}
