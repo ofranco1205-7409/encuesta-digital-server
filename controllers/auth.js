f7b54c601e50ce50187a18c8cd5ae88be4d4d4b1
Omar Franco Garcia <ofranco1205@gmail.com>
2023-01-04 12:30:49 -0600

    Add hello endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,34 +2,34 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const jwt = require("../utils/jwt");
 
-function register(req, res){
+function register(req, res) {
   const { firstname, lastname, email, password } = req.body;
 
   if (!email) res.status(400).send({ msg: "El email es obligatorio" });
   if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
 
-    const user=new User ({
+  const user = new User({
     firstname,
     lastname,
-     email:email.toLowerCase(),
-     role:"user",
-     active:false,     
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
   });
 
   const salt = bcrypt.genSaltSync(10);
   const hashPassword = bcrypt.hashSync(password, salt);
   user.password = hashPassword;
 
-    user.save((error,userStorage)=>{
-        if (error){
+  user.save((error, userStorage) => {
+    if (error) {
       res.status(400).send({ msg: "Error al crear el usuario" });
-        }else{
+    } else {
       res.status(200).send(userStorage);
-        }
+    }
   });
-}  
+}
 
-function login(req, res){
+function login(req, res) {
   const { email, password } = req.body;
 
   if (!email) res.status(400).send({ msg: "El email es obligatorio" });
@@ -38,47 +38,52 @@ function login(req, res){
   const emailLowerCase = email.toLowerCase();
 
   User.findOne({ email: emailLowerCase }, (error, userStore) => {
-        if (error){
+    if (error) {
       res.status(500).send({ msg: "Error del servidor" });
-        }else{
+    } else {
       bcrypt.compare(password, userStore.password, (bcryptError, check) => {
-                if (bcryptError){
+        if (bcryptError) {
           res.status(500).send({ msg: "Error del servidor" });
-                }else if (!check){
+        } else if (!check) {
           res.status(400).send({ msg: "Contraseña incorrecta" });
         } else if (!userStore.active) {
           res.status(401).send({ msg: "Usuario no autorizado o no activo" });
-                }else{
-                    res.status(200).send({
+        } else {
+          res.status(200).send({
             access: jwt.createAccessToken(userStore),
             refresh: jwt.createRefreshToken(userStore),
           });
-                }
-      });
         }
+      });
+    }
   });
 }
 
-function refreshAccessToken(req,res){
+function refreshAccessToken(req, res) {
   const { token } = req.body;
 
   if (!token) res.status(400).send({ msg: "Token requerido" });
 
   const { user_id } = jwt.decoded(token);
 
-    User.findOne({_id:user_id},(error,userStorage)=>{
-        if (error){
+  User.findOne({ _id: user_id }, (error, userStorage) => {
+    if (error) {
       res.status(500).send({ msg: "Error del servidor" });
-        }else{
-            res.status(200).send({
+    } else {
+      res.status(200).send({
         accessToken: jwt.createAccessToken(userStorage),
       });
-        }
+    }
   });
 }
 
-module.exports={
-    register,
-    login,
+function hello(req, res) {
+  res.status(200).send({ msg: "Hello" });
+}
+
+module.exports = {
+  register,
+  login,
   refreshAccessToken,
+  hello,
 };
